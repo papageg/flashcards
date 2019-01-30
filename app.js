d741695211290171aac89beb8c5a0bb855444463
Greg <greggordon98@gmail.com>
2019-01-30 12:13:16 -0800

    done
    done

--- app.js
+++ app.js
@@ -30,5 +30,3 @@ app.use((err, req, res, next) => {
 app.listen(3000, () => {
   console.log('The application is running on localhost:3000!')
 });
-
-console.log('Greg');
--- routes/cards.js
+++ routes/cards.js
@@ -1,13 +1,40 @@
+//import { templateElement } from 'babel-types';
+
 const express = require('express');
 const router = express.Router();
 const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
-router.get('/', (req, res) => {
-  res.render('card', { 
-    prompt: cards[0].question,
-    hint: cards[0].hint
-   });
+router.get('/', ( req, res ) => {
+  const numberOfCards = cards.length;
+  const flashcardId = Math.floor( Math.random() * numberOfCards );
+  res.redirect(`/cards/${flashcardId}` )
 });
+
+router.get('/:id', (req, res) => {
+  const { side } = req.query;
+  const { id } = req.params;
+
+  if( !side ) {
+    res.redirect(`/cards/${id}?side=question`);
+  }
+  const name = req.cookies.username;
+  const text = cards[id][side];
+  const { hint } = cards[id];
+
+  const templateData = { id, text, name };
+
+  if(side === 'question') {
+    templateData.hint = hint;
+    templateData.sideToShow = 'answer';
+    templateData.sideToShowDisplay = 'Answer';
+  } else if (side === 'answer') {
+    templateData.sideToShow= 'question';
+    templateData.sideToShowDisplay= 'Question';
+  }
+
+  res.render('card', templateData);
+});
+
 
 module.exports = router;
